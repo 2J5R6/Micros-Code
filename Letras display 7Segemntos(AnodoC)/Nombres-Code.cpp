73cf60347e1fc6e44d84b0f81ea32355e511b9d6
DanielAraqueStudios <74468283+DanielAraqueStudios@users.noreply.github.com>
2025-02-09 23:00:08 -0500

    Update Nombres-Code.cpp

--- Letras display 7Segemntos(AnodoC)/Nombres-Code.cpp
+++ Letras display 7Segemntos(AnodoC)/Nombres-Code.cpp
@@ -1,5 +1,12 @@
 #include <stdio.h>
-#include <stm32f7xx.h>
+#include <stm32f4xx.h>
+
+
+
+
+
+int t =1000000;
+
 
 void configurarPuertoA(){
 
@@ -18,99 +25,353 @@ void configurarPuertoA(){
         GPIOA->PUPDR |= 0xAAA; 
     }
     
-    
+    //SETTING GPIOS FOR LETTERS
+    void letraA() {
+        GPIOA->ODR = 0x8; //Dejo pine 3 en 1
+    }
+
+    void letraB() {
+        GPIOA->ODR = (1<<0)| (1<<1);
+    }
+
+    void letraC() {
+        GPIOA->ODR = (1<<1) | (1<<2) |(1<<6);
+    }
+
+    void letraD() {
+        GPIOA->ODR = (1<<5)|(1<<0);
+    }
+
+    void letraE() {
+        GPIOA->ODR = (1<<1)|(1<<2);
+    }
+
+    void letraF() {
+        GPIOA->ODR = (1<<1) | (1<<2) | (1<<3) ;
+    }
+
+    void letraG() {
+        GPIOA->ODR = (1<<4);
+    }
+
+    void letraH() {
+        GPIOA->ODR = (1<<0) | (1<<3) ;
+    }
+
+    void letraI() {
+        GPIOA->ODR = (1<<5)|(1<<1)|(1<<6)|(1<<2); // Dejo pines 1,2,5 y 6 en 1
+    }
+
     void letraJ() {
         GPIOA->ODR = 0x71; // Dejo los pines 0, 4, 5 y 6 en 1
     }
-    
-    void letraU() {
-        GPIOA->ODR = 0x63; // Dejo pines 0,1,5,6 en 1
+
+    void letraK() {
+        GPIOA->ODR = (1<<0)|(1<<2);
     }
-    
+
     void letraL() {
         GPIOA->ODR = 0x47; // Dejo pines 0,1,2 y 6 en 1
     }
     
-    void letraI() {
-        GPIOA->ODR = 0x66; // Dejo pines 1,2,5 y 6 en 1
+    void letraM() {
+        GPIOA->ODR = (1<<3) | (1<<6)  ;
     }
-    
-    void letraA() {
-        GPIOA->ODR = 0x8; //Dejo pine 3 en 1
-    }
-    
+
     void letraN() {
-        GPIOA->ODR = 0xB; //Dejo pines 0,1,3 y 5 en 1
+        GPIOA->ODR = (1<<5)|(1<<0)|(1<<1)|(1<<3); //Dejo pines 0,1,3 y 5 en 1
     }
-    
-    void letraD() {
-        GPIOA->ODR = 0x3E;
-    }
-    
-    void letraR() {
-        GPIOA->ODR = 0x50;
-    }
-    
-    void letraE() {
-        GPIOA->ODR = 0x79;
-    }
-    
-    void letraS() {
-        GPIOA->ODR = 0x6D;
-    }
-    
+
     void letraO() {
-        GPIOA->ODR = 0x3F;
+        GPIOA->ODR = (1<<6);
     }
-    
-    void letraG() {
-        GPIOA->ODR = 0x3D;
+
+    void letraP() {
+        GPIOA->ODR =   (1<<3) |  (1<<2) ;
     }
-    
-    void letraC() {
-        GPIOA->ODR = 0x39;
-    }
-    
+
     void letraQ() {
-        GPIOA->ODR = 0x3F;
+        GPIOA->ODR = (1<<3)|(1<<4);
     }
-    
+
+    void letraR() {
+        GPIOA->ODR = (1<<0)|(1<<1)|(1<<2)|(1<<3)|(1<<5);
+    }
+
+    void letraS() {
+        GPIOA->ODR = (1<<4)|(1<<1);
+    }
+
     void letraT() {
-        GPIOA->ODR = 0x78;
+        GPIOA->ODR = (1<<0)|(1<<1)|(1<<2);
     }
-    
-    void letraK() {
-        GPIOA->ODR = 0x75;
+
+    void letraU() {
+        GPIOA->ODR = 0x63; // Dejo pines 0,1,5,6 en 1
     }
-    
+
     void letraV() {
         GPIOA->ODR = 0x3E;
     }
+
+
     
-    void letraB() {
-        GPIOA->ODR = 0x7C;
+    //SETTING TIME FOR LETTERS TO BE DISPLAYED
+		
+		void  timeA(){
+			
+			for (int i=0; i<t; i++){
+				
+				letraA();
+				
+				
+				
+			}				
+		}
+        
+        void  timeB(){
+			
+			for (int i=0; i<t; i++){
+				
+				letraB();
+				
+				
+				
+			}				
+		}
+        
+        void  timeC(){
+            for (int i=0; i<t; i++){
+				
+				letraC();
+				
+				
+				
+			}	
+        }
+
+        void  timeD(){
+            for (int i=0; i<t; i++){
+                letraD();
+            }
+     }
+
+
+     void  timeE (){
+        for (int i=0; i<t; i++){
+            
+            letraE();
+            
+            
+            
+        }	
     }
-    
-    
-    void delay(int milliseconds) {
-        volatile int count = milliseconds * 1000;
-        while (count--);
+
+    void  timeF(){
+        for (int i=0; i<t; i++){
+            letraF();
+        }
     }
+
+    void  timeG(){
+        for (int i=0; i<t; i++){
+            letraG();
+        }
+    }
+
+    void  timeH(){
+        for (int i=0; i<t; i++){
+            letraH();
+        }
+    }
+
+    void  timeI(){
+        for (int i=0; i<t; i++){
+            letraI();
+        }
+    }
+
+    void  timeJ(){
+        for (int i=0; i<t; i++){
+            letraJ();
+        }
+    }
+
+    void  timeK(){
+        for (int i=0; i<t; i++){
+            letraK();
+        }
+    }
+
+    void  timeL(){
+        for (int i=0; i<t; i++){
+            letraL();
+        }
+    }
+
+    void  timeM(){
+        for (int i=0; i<t; i++){
+            letraM();
+        }
+    }
+
+    void  timeN(){
+        for (int i=0; i<t; i++){
+            letraN();
+        }
+    }
+
+    void  timeO(){
+        for (int i=0; i<t; i++){
+            letraO();
+        }
+    }
+
+    void  timeP(){
+        for (int i=0; i<t; i++){
+            letraP();
+        }
+    }
+
+    void  timeQ(){
+        for (int i=0; i<t; i++){
+            letraQ();
+        }
+    }
+
+    void  timeR(){
+        for (int i=0; i<t; i++){
+            letraR();
+        }
+    }
+
+    void  timeS(){
+        for (int i=0; i<t; i++){
+            letraS();
+        }
+    }
+
+    void  timeT(){
+        for (int i=0; i<t; i++){
+            letraT();
+        }
+    }
+
+    void  timeU(){
+        for (int i=0; i<t; i++){
+            letraU();
+        }
+    }
+
+    /*
+
+    void  timeV(){
+        for (int i=0; i<t; i++){
+            letraV();
+        }
+    }
+
+    void  timeW(){
+        for (int i=0; i<t; i++){
+            letraW();
+        }
+    }
+
+    void  timeX(){
+        for (int i=0; i<t; i++){
+            letraX();
+        }
+    }   
+
+    void  timeY(){
+        for (int i=0; i<t; i++){
+            letraY();
+        }
+    }
+
+    void  timeZ(){
+        for (int i=0; i<t; i++){
+            letraZ();
+        }
+    }
+
+    */
+
+
+
+//names
+
+void julian(){
+	
+	timeJ();
+	timeU();
+	timeL();
+	timeI();
+	timeA();
+	timeN();
+}
+
+
+void daniel(){
+	
+	timeD();
+	timeA();
+	timeN();
+	timeI();
+	timeE();
+	timeL();
+}
+
+void andres(){
+	
+	
+	timeA();
+	timeN();
+	timeD();
+	timeR();
+	timeE();
+	timeS();
+	
+}
+
+
 
     int main() {
         //------Configuro puerto A------
         configurarPuertoA();
 
-/*        while(1){
-            void (*letras[])() = {letraJ, letraU, letraL, letraI, letraA, letraN};
-            for (int i = 0; i < 6; ++i) {
-                letras[i]();
-				}
-*/					
+        while(1){
+					
+					
+					
+					julian();
+					daniel();
+					andres();
+           					
+										timeA();					
+                    timeB();
+										timeC();
+										timeD();                
 
-			 while(1){
-			 GPIOA->ODR = (1<<3);
-			 }
-						
-}
-       
+                    timeE();
+                    timeF();
+                    timeG();    
+                    timeH();
+                    timeI();
+                    timeJ();
+                    timeK();
+                    timeL();
+                    timeM();
+                    timeN();
+                    timeO();
+                    timeP();
+                    timeQ();
+                    timeR();
+                    timeS();
+                    timeT();
+                    timeU();
+										
+                    
+					
+        }
+    }
